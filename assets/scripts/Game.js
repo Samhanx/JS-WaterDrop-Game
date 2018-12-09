cf09b028c65b023b1a5168189bef03d9650c8571
samhanx <xuanxinhan@vip.qq.com>
2018-12-09 22:16:56 +0800

    refactor: extract onClick and onBoom

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -21,50 +21,52 @@ export default {
     let game = this
     game._setGameLifeDisplay()
 
+    const onClick = function() {
+      if (game.life > 0) {
+        game.life--
+        game._setGameLifeDisplay()
+        this.levelUp()
+      }
+    }
+    const onBoom = function() {
+      let self = this
+      const onMove = function() {
+        let canCollideWaterDrops = game.waterDrops.filter(waterDrop => {
+          return waterDrop.level > 0 && waterDrop !== self
+        })
+        let collidedWaterDrop = this.checkCollision(canCollideWaterDrops)
+        if (collidedWaterDrop) {
+          // 碰撞销毁
+          this.destroy()
+          game.bullets = game.bullets.filter(bullet => bullet !== this)
+          collidedWaterDrop.levelUp()
+        } else if (this.left < -this.width || this.left > game.boardWrapper.offsetWidth || this.top < -this.height || this.top > game.boardWrapper.offsetHeight) {
+          // 超出左边 this.left < -this.width, 超出右边 this.left > game.boardWrapper.offsetWidth
+          // 超出上边 this.top < -this.height, 超出下边 this.top > game.boardWrapper.offsetHeight
+          this.destroy()
+          game.bullets = game.bullets.filter(bullet => bullet !== this)
+        }
+      }
+
+      let directions = ['left', 'top', 'right', 'bottom']
+      directions.forEach(direction => {
+        let buttet = new Bullet({
+          direction,
+          speed: 3,
+          onMove,
+        })
+        game.bullets.push(buttet)
+        buttet.draw(game.boardWrapper)
+        buttet.setPosition(this.left, this.top)
+      })
+    }
+
     for (let i = 0; i < 36; i++) {
       let waterDrop = new WaterDrop({
         level: game._getRandomGameSeed(),
-        onClick() {
-          if (game.life > 0) {
-            game.life--
-            game._setGameLifeDisplay()
-            this.levelUp()
-          }
-        },
-        onBoom() {
-          let self = this
-          const onMove = function() {
-            let canCollideWaterDrops = game.waterDrops.filter(waterDrop => {
-              return waterDrop.level > 0 && waterDrop !== self
-            })
-            let collidedWaterDrop = this.checkCollision(canCollideWaterDrops)
-            if (collidedWaterDrop) {
-              // 碰撞销毁
-              this.destroy()
-              game.bullets = game.bullets.filter(bullet => bullet !== this)
-              collidedWaterDrop.levelUp()
-            } else if (this.left < -this.width || this.left > game.boardWrapper.offsetWidth || this.top < -this.height || this.top > game.boardWrapper.offsetHeight) {
-              // 超出左边 this.left < -this.width, 超出右边 this.left > game.boardWrapper.offsetWidth
-              // 超出上边 this.top < -this.height, 超出下边 this.top > game.boardWrapper.offsetHeight
-              this.destroy()
-              game.bullets = game.bullets.filter(bullet => bullet !== this)
-            }
-          }
-
-          let directions = ['left', 'top', 'right', 'bottom']
-          directions.forEach(direction => {
-            let buttet = new Bullet({
-              direction,
-              speed: 3,
-              onMove,
-            })
-            game.bullets.push(buttet)
-            buttet.draw(game.boardWrapper)
-            buttet.setPosition(this.left, this.top)
-          })
-        },
+        onClick,
+        onBoom,
       })
-
       game.waterDrops.push(waterDrop)
       waterDrop.draw(game.boardWrapper)
     }
