fc0b584b3c02c92d37bd27ba15a39e1b1b721de6
samhanx <xuanxinhan@vip.qq.com>
2018-12-09 17:35:14 +0800

    feat: water drop level up when clicked

--- assets/scripts/WaterDrop.js
+++ assets/scripts/WaterDrop.js
@@ -1,20 +1,31 @@
 export default class {
   constructor(level) {
-    this.div = null
-    this.img = null
+    this.container = null
+    this.drop = null
     this.level = level
   }
 
   draw(wrapper) {
-    this.div = document.createElement('div')
-    this.img = new Image()
-    this.div.classList.add('water-drop-box')
-    this.img.classList.add('water-drop')
-    this.img.src = `/assets/images/${this.level}.png`
-    this.img.onload = () => {
-      this.div.appendChild(this.img)
+    this.container = document.createElement('div')
+    this.drop = new Image()
+    this.container.classList.add('water-drop-box')
+    this.drop.classList.add('water-drop')
+    this.drop.src = `/assets/images/${this.level}.png`
+    this.drop.onload = () => {
+      this.container.appendChild(this.drop)
+    }
+    this.drop.onclick = this.levelUp.bind(this)
+    
+    wrapper.appendChild(this.container)
+  }
+
+  levelUp() {
+    if (this.level === 4) {
+      this.level = 0
+    } else {
+      this.level++
     }
     
-    wrapper.appendChild(this.div)
+    this.drop.src = `/assets/images/${this.level}.png`
   }
 }
