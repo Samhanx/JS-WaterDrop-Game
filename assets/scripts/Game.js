7eb11ecfb968231a50e6fc4cb3adbf57d60e5f91
samhanx <xuanxinhan@vip.qq.com>
2018-12-09 23:03:43 +0800

    refactor: extract functions

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -18,39 +18,33 @@ export default {
 
   start() {
     if (!this.boardWrapper || !this.countWrapper || !this.waterWrapper) throw new Error('DOM Container does not initialized for Game!')
-    let game = this
-    game._setGameLifeDisplay()
+    this._setGameLifeDisplay()
+    const onClick = this._waterDropOnClick.call(null, this)
+    const onBoom = this._waterDropOnBoom.call(null, this)
+    for (let i = 0; i < 36; i++) {
+      let waterDrop = new WaterDrop({
+        level: this._getRandomGameSeed(),
+        onClick,
+        onBoom,
+      })
+      this.waterDrops.push(waterDrop)
+      waterDrop.draw(this.boardWrapper)
+    }
+  },
 
-    const onClick = function() {
+  _waterDropOnClick(game) {
+    return function() {
       if (game.life > 0) {
         game.life--
         game._setGameLifeDisplay()
         this.levelUp()
       }
     }
-    const onBoom = function() {
-      let self = this
-      const onMove = function() {
-        let canCollideWaterDrops = game.waterDrops.filter(waterDrop => {
-          return waterDrop.level > 0 && waterDrop !== self
-        })
-        let collidedWaterDrop = this.checkCollision(canCollideWaterDrops)
-        if (collidedWaterDrop) {
-          // 碰撞销毁
-          this.destroy()
-          game.bullets = game.bullets.filter(bullet => bullet !== this)
-          collidedWaterDrop.levelUp()
-        } else if (this.left < -this.width || this.left > game.boardWrapper.offsetWidth || this.top < -this.height || this.top > game.boardWrapper.offsetHeight) {
-          // 超出左边 this.left < -this.width, 超出右边 this.left > game.boardWrapper.offsetWidth
-          // 超出上边 this.top < -this.height, 超出下边 this.top > game.boardWrapper.offsetHeight
-          this.destroy()
-          game.bullets = game.bullets.filter(bullet => bullet !== this)
-        }
-        if (game.bullets.length === 0) {
-          game._checkGame()
-        }
-      }
+  },
 
+  _waterDropOnBoom(game) {
+    return function() {
+      const onMove = game._bulletOnMove.call(null, game, this)
       let directions = ['left', 'top', 'right', 'bottom']
       directions.forEach(direction => {
         let buttet = new Bullet({
@@ -63,15 +57,28 @@ export default {
         buttet.setPosition(this.left, this.top)
       })
     }
+  },
 
-    for (let i = 0; i < 36; i++) {
-      let waterDrop = new WaterDrop({
-        level: game._getRandomGameSeed(),
-        onClick,
-        onBoom,
+  _bulletOnMove(game, waterDropSelf) {
+    return function() {
+      let canCollideWaterDrops = game.waterDrops.filter(waterDrop => {
+        return waterDrop.level > 0 && waterDrop !== waterDropSelf
       })
-      game.waterDrops.push(waterDrop)
-      waterDrop.draw(game.boardWrapper)
+      let collidedWaterDrop = this.checkCollision(canCollideWaterDrops)
+      if (collidedWaterDrop) {
+        // 碰撞销毁
+        this.destroy()
+        game.bullets = game.bullets.filter(bullet => bullet !== this)
+        collidedWaterDrop.levelUp()
+      } else if (this.left < -this.width || this.left > game.boardWrapper.offsetWidth || this.top < -this.height || this.top > game.boardWrapper.offsetHeight) {
+        // 超出左边 this.left < -this.width, 超出右边 this.left > game.boardWrapper.offsetWidth
+        // 超出上边 this.top < -this.height, 超出下边 this.top > game.boardWrapper.offsetHeight
+        this.destroy()
+        game.bullets = game.bullets.filter(bullet => bullet !== this)
+      }
+      if (game.bullets.length === 0) {
+        game._checkGame()
+      }
     }
   },
 
